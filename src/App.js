3ea1584f34d20c52229369d8aadb1a1d1c15cdf7
colin richards <colin.rhys.richards@gmail.com>
2021-05-10 19:19:03 -0600

    started mobile navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import HomepageView from './components/HomepageView'
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+    <Navbar />
     <HomepageView />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from 'styled-components'
+import Icon from '../icons/R&RIcon.svg'
+
+
+const Div = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    background: #03022D;
+    color: white;
+` 
+const Logo = styled.img`
+    height: auto;
+    width: 20%;
+    padding: 13px 0px;
+    max-width: 600px;
+    height: auto;
+    overflow: hidden;
+    text-align: center;
+    margin: 0 21px;
+`
+const HamburgerMenu = styled.div`
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+    overflow: hidden;
+    text-align: center;
+    margin: 0 20px;
+    padding: 1rem;
+`
+
+function Navbar() {
+    return (
+      <Div>
+        <Logo src={Icon} alt="homepage icon" />
+        <Logo src={Icon} alt="homepage icon" />
+        {/* <HamburgerMenu>H</HamburgerMenu> */}
+      </Div>
+    );
+  }
+
+  export default Navbar;
